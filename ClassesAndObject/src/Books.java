0361658bc3ed339e9b142a8c18360a8209d8b195
alopez651 <ana.lopez6517@gmail.com>
2022-08-04 14:38:15 -0700

    adding thursdays exercise

---
+++ ClassesAndObject/src/Books.java
@@ -0,0 +1,69 @@
+public class Books {
+    String name;
+    String author;
+    String year;
+    int personal_rating;
+    String genre;
+    String pages;
+    String language;
+    String names;
+
+
+    public Books(String name, String author, String genre,
+                 String year, int personal_rating){
+        this.name = name;
+        this.author = author;
+        this.genre = genre;
+        this.year = year;
+        this.personal_rating= personal_rating;
+    }
+    public Books(String name, String author, String genre,
+                 String year, int personal_rating, int pages){
+        this.name = name;
+        this.author = author;
+        this.genre = genre;
+        this.year = year;
+        this.personal_rating= personal_rating;
+        this.pages = ", the total pages for all books is " + pages;
+    }
+    public Books(String name, String genre,
+                 String year, int personal_rating, String language){
+        this.name = name;
+        this.author= " are multiple since book has been translated multiple time for the past years, ";
+        this.genre = genre;
+        this.year = year;
+        this.personal_rating= personal_rating;
+        this.language = language;
+    }
+//    public Books(String names){
+//        this.names = names;
+//    }
+
+    @Override
+    public String toString(){
+        String returnValue = "";
+
+        returnValue += "The name of the book is " + this.name;
+        returnValue += "\nwritten by " + this.author;
+        returnValue += "\nthe genre of the book is " + this.genre;
+        returnValue += "\nthe book was publish in " + this.year;
+        returnValue += "\nmy personal rating for this book is " + this.personal_rating + "/10";
+//        returnValue += "\nthe book was originally written in  " + this.language;
+        returnValue += "\nand" + this.pages;
+//        returnValue += "\nBooks I recommend to read: " + this.names;
+        return returnValue;
+    }
+
+    @Override
+    public boolean equals(Object book){
+        Books other = (Books) book;
+
+        if(!(this.author.equals(other.author))) return false;
+
+        return true;
+    }
+
+
+
+
+}
---
+++ ClassesAndObject/src/Obj.java
@@ -0,0 +1,26 @@
+import java.awt.print.Book;
+
+public class Obj {
+    public static void main(String[] args){
+        Books book1 = new Books("Crime and Punishment", "Literary Fiction",
+                "1866", 9, "Russian");
+        Books book2 = new Books("Little Women", "Louisa May Alcott","Novel",
+                "1868", 8 );
+        Books book3 = new Books("Harry Potter", "J.K Rowling","Fantasy",
+                "1997 - 2007", 10, 4100 );
+        Books book4 = new Books("Tender is the Night", "F. Scott Fitzgerald",
+                "Tragedy", "1934", 9 );
+        Books book5 = new Books("Cold Blood", "Trueman Capote", "Nonfiction/Literacy",
+                "1965", 8);
+        Books book6 = new Books("The Great Gatsby", "F. Scott Fitzgerald", "Historical Fiction",
+                "1925", 7);
+
+
+
+
+        System.out.println(book3.toString());
+        System.out.println("Cold blood to Crime and punishment: " + book1.equals(book5));
+        System.out.println("Tender is the Night and The Great Gatsby: " + book4.equals(book6));
+
+    }
+}
